408455bbf078d9b6d14afe0ec83400d36fea1fe3
Mike Bourgeous <mike@mikebourgeous.com>
2003-09-20 06:53:00 +0000

    Fifth pass on dma load descriptor function.  No more goto.

--- driver/tl880dma.c
+++ driver/tl880dma.c
@@ -116,20 +116,14 @@ void tl880_aux_load_dma_mbox(struct tl880_dev *tl880dev, struct tl880_dma_info *
 	write_register(tl880dev, 0x25728, dmainfo->field_4);
 }
 
-// #if 0
-/* 
- * I suspect this function has other functions inlined into it, or it had a lot
- * of duplicate code that was optimized away, as it has a really convoluted
- * execution flow through it.
- */
-void VopLoadDescriptor(struct tl880_dev *tl880dev, int arg_0, int arg_4)
+void tl880_load_dma_descriptor(struct tl880_dev *tl880dev, int arg_0, int arg_4)
 {
 	struct tl880_dma_info vop_dma_info;
 	struct tl880_dma_info tsd_dma_info;
-	unsigned long eax = 0, ecx = 0, edx = 0;
-	unsigned long esi = 0, edi = 0;
+	unsigned long eax = 0, ecx = 0;
+	unsigned long edi = 0;
 	unsigned long CDma[0x100];
-	unsigned long var_4;
+	unsigned long aux_dmaphys;
 	unsigned long tsdDmaMode = 0, tsdDmaActive = 0;
 	unsigned long g_tsDMAenabled = 0;
 	unsigned long tsDmaBufCnt = 0, tsDmaFullness = 0;
@@ -138,16 +132,16 @@ void VopLoadDescriptor(struct tl880_dev *tl880dev, int arg_0, int arg_4)
 	/* eax = (unsigned long)&CDma[(arg_4 << 4) + 4]; */
 	
 	if(CDma[0x34] == 0 || CDma[0x3c] == 0) {
-		/* eax = *(unsigned long *)(eax+4); */
-		var_4 = CDma[arg_4 * 16 + 8];
+		/* aux_dmaphys = *(unsigned long *)(eax+4); */
+		aux_dmaphys = CDma[arg_4 * 16 + 8];
 	} else {
-		/* eax = *(unsigned long *)(eax+8); */
-		var_4 = CDma[arg_4 * 16 + 0xc];
+		/* aux_dmaphys = *(unsigned long *)(eax+8); */
+		aux_dmaphys = CDma[arg_4 * 16 + 0xc];
 	}
 
 	if(g_tsDMAenabled && (arg_4 || !(CDma[0x38] || CDma[0x3c]))) {
 		if(tsDmaBufCnt == 0) {
-			eax = 0 /* GetRawStreamCounter() */;
+			/* GetRawStreamCounter(); */
 		}
 		
 		/* 0x5e00 is 128 ts packets of 188 bytes each */
@@ -155,25 +149,10 @@ void VopLoadDescriptor(struct tl880_dev *tl880dev, int arg_0, int arg_4)
 		
 		tsDmaFullness = 0 /* RawStreamBufFullness() / 0x5e00 */;
 
-		/*
-		if(tsDmaFullness == 0) {
-			return;
-		}
-		
-		if(tsDmaBufCnt != 0) {
-			goto loc_25aa1;
-		}
-		
-		if(tsDmaFullness >= 7) {
-			return;
-		}
-		*/
-
 		if(tsDmaFullness == 0 || (tsDmaBufCnt == 0 && tsDmaFullness >= 7)) {
 			return;
 		}
 		
-loc_25aa1:
 		edi = tsDmaFullness * 0x5e00;
 		
 		ecx = 0x2f000 - 0 /* GetRawStreamRdPtr(1) */;
@@ -184,13 +163,15 @@ loc_25aa1:
 		
 		tsd_dma_info.field_10 = 0xff04;
 		
-		eax = 0 /* GetRawStreamRdPtr(0) */;
-		
+		tsd_dma_info.field_14 = 0 /* GetRawStreamRdPtr(0) */;
+	
+#if 0
 		ecx = g_tsBufWrPtr;
-		tsd_dma_info.field_14 = eax;
 		eax = tl880dev->dmaphys;	/* g_rawStreamPhyAddr */
 		ecx += eax;
 		tsd_dma_info.phys_addr = ecx;
+#endif
+		tsd_dma_info.phys_addr = tl880dev->dmaphys + g_tsBufWrPtr;
 		
 		tsdDmaMode = 0;
 		tsd_dma_info.field_8 = 0x5e00;
@@ -200,115 +181,51 @@ loc_25aa1:
 		
 		tl880_aux_load_dma_mbox(tl880dev, &tsd_dma_info);
 	} else { 
-loc_25b29_auxdma:
 		tsdDmaMode = 1;
-		if(eax != 0) {
-			goto loc_25bd2;
-		}
-		
-		/* eax = CDma[0x38]; */
-		if(CDma[0x38] == 0) {
-			goto loc_25ba2;
-		}
-		
-		if(CDma[0x3c] == 0) {
-			goto loc_25b80;
-		}
-		
-		if(CDma[0x50] != 2) {
-			goto loc_25b62;
-		}
-		
-		if(arg_4 == 0) {
-			goto loc_25b62;
-		}
-		
-		CDma[0x28] = CDma[0x60];
-		CDma[0x2c] = CDma[0x64];
-		eax = CDma[0x68];
-		CDma[0x5c] = eax;
-		goto loc_25b71;
-		
-loc_25b62:
-		CDma[0x28] = CDma[0x6c];
-		CDma[0x2c] = CDma[0x70];
-		eax = CDma[0x74];
-		CDma[0x5c] = eax;
-		
-loc_25b71:
-		eax = CDma[0x2c] + 3;
-		
-		/* SetupAuxVideo(CDma[0x28], eax); */
-		goto loc_25bd2;
-		
-loc_25b80:
-		if(CDma[0x38] != 0) {
-			ecx = CDma[0x60];
-			if(CDma[0x28] != CDma[0x60]) {
-				goto loc_25b94;
+		if(!aux_dmaphys) {
+			if(CDma[0x38] != 0) { 
+				if(CDma[0x3c]) {
+					if(CDma[0x50] != 2 || arg_4 == 0) {
+						CDma[0x28] = CDma[0x6c];
+						CDma[0x2c] = CDma[0x70];
+						CDma[0x5c] = CDma[0x74];
+					} else {
+						CDma[0x28] = CDma[0x60];
+						CDma[0x2c] = CDma[0x64];
+						CDma[0x5c] = CDma[0x68];
+					}
+					
+					/* SetupAuxVideo(CDma[0x28], CDma[0x2c] + 3); */
+				} else if(CDma[0x38] != 0) {
+					ecx = CDma[0x60];
+					if(CDma[0x28] != CDma[0x60] || CDma[0x2c] != CDma[0x64]) {
+						CDma[0x28] = CDma[0x60];
+						CDma[0x2c] = CDma[0x64];
+						CDma[0x5c] = CDma[0x68];
+						
+						/* SetupAuxVideo(CDma[0x60], CDma[0x64] + 3); */
+					}
+				}
+			} else if(CDma[0x3c] != 0) {
+				if(CDma[0x28] != CDma[0x6c] || CDma[0x2c] != CDma[0x70]) {
+					CDma[0x28] = CDma[0x6c];
+					CDma[0x2c] = CDma[0x70];
+					CDma[0x5c] = CDma[0x74];
+					
+					/* SetupAuxVideo(CDma[0x6c], CDma[0x70] + 3); */
+				}
 			}
-			
-			eax = CDma[0x2c];
-			if(CDma[0x2c] == CDma[0x64]) {
-				goto loc_25bd2;
-			}
-			
-loc_25b94:
-			eax = CDma[0x64];
-			edx = CDma[0x68];
-			CDma[0x28] = CDma[0x60];
-			CDma[0x2c] = CDma[0x64];
-			CDma[0x5c] = edx;
-			goto loc_25bc3;
-		}
-
-		
-loc_25ba2:
-		if(CDma[0x3c] == 0) {
-			goto loc_25bd2;
 		}
 		
-		ecx = CDma[0x6c];
-		eax = CDma[0x2c];
-		/*
-		if(CDma[0x28] != CDma[0x6c]) {
-			goto loc_25bb7;
-		}
-		
-		if(CDma[0x2c] == CDma[0x70]) {
-			goto loc_25bd2;
-		}
-		*/
-
-		if(CDma[0x28] != CDma[0x6c] || CDma[0x2c] != CDma[0x70]) {
-			goto loc_25bb7;
-loc_25bb7:
-			eax = CDma[0x70];
-			edx = CDma[0x74];
-			CDma[0x28] = CDma[0x6c];
-			CDma[0x2c] = CDma[0x70];
-			CDma[0x5c] = edx;
-		} else {
-			goto loc_25bd2;
-		}
-		
-loc_25bc3:
-		eax += 3;
-		
-		/* SetupAuxVideo(ecx, eax); */
-		
-loc_25bd2:
-		vop_dma_info.phys_addr = var_4;
+		vop_dma_info.phys_addr = aux_dmaphys;
 		vop_dma_info.field_4 = CDma[0x2c] / CDma[0x5c];
 		vop_dma_info.field_8 = CDma[0x28] * CDma[0x5c];
 		vop_dma_info.field_c = (CDma[0x28] * CDma[0x5c]) / 2;
 		vop_dma_info.field_10 = 0xff14;
 		tsdDmaActive = 1;
 		
-loc_25c03:
 		tl880_aux_load_dma_mbox(tl880dev, &vop_dma_info);
 	}
-loc_25c08:
 }
-// #endif
+
 
